12de86a84c3b4a7abe453cfc3535343d77d2a266
Oksana Vyshinskaya <oksana.vyshinskaya@progforce.com>
2023-09-19 09:55:28 +0300

    nocache 2

--- index.js
+++ index.js
@@ -16,7 +16,20 @@ app.use(
     origin: true,
   })
 );
+app.use(nocache());
+app.disable("view cache");
+app.use((req, res, next) => {
+  res.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  res.set("Pragma", "no-cache");
+  res.set("Expires", "0");
+  res.set("Surrogate-Control", "no-store");
+  res.set("fetchCache", "force-no-store");
 
+  next();
+});
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,7 +38,5 @@ app.use(require("./routes/index.routes"));
 app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
 });
-app.use(nocache());
-app.disable("view cache");
 
 app.listen("1337");
